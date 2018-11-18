ac052d200a20c562ff002c41bafebad531b8ac67
gpabdo <gpabdo@gmail.com>
2018-11-17 22:48:06 -0800

    Add args

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
+# How do you talk to an arduino
+  
+## Install 
+```
+$ pip install .
+```
+
 # put the init service in:
 /etc/init.d/
 
 # Put the Python script in:
 /usr/bin/
-
-# Put arduino.py in:
-/usr/lib/python2.7/
---
+++ environment/environment.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+
+import sys
+import argparse 
+import json
+import time
+import requests
+from arduinoCommunication import arduinoSerial
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+#
+# Oh where is the ardunio?
+#
+arduino = arduinoSerial()
+
+producer = KafkaProducer(bootstrap_servers=['kafka.pad.the-collective-group.com:9092'], value_serializer=lambda v: json.dumps(v).encode('ascii'))
+
+
+## ----------------------------------------- ##
+#
+## ----------------------------------------- ##
+def main(argv):
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-s', '--site',
+                    required=True,
+                    dest="site",
+                    help="Geographic site of the weatherstation" )
+
+  parser.add_argument('-l', '--location',
+                    required=True,
+                    dest="location",
+                    help="Room the weatherstatoin is in" )
+ 
+  args = parser.parse_args()
+  collect( site=args.site, location=args.location )
+
+
+## ----------------------------------------- ## 
+#
+## ----------------------------------------- ## 
+def collect( site="default", location="default" ):
+  print "Starting loop"
+  while( True ):
+
+     data = {}
+     event = {}
+
+     data['command'] = 'getParticles'
+     arduino.write(json.dumps(data))
+    
+     event['pm'] = json.loads(arduino.read())
+     if event['pm']["status"] != "OK":
+       print "ERROR: " + str( event['pm']["status"] )
+       continue 
+
+     data['command'] = 'getWeather'
+     arduino.write(json.dumps(data))
+
+     event['weather'] = json.loads(arduino.read())
+     if event['weather']["status"] != "OK":
+       print "ERROR: " + str( event['weather']["status"] )
+       continue
+
+     event["site"] = site 
+     event["location"] = location 
+     
+     producer.send('environment', event)
+
+     time.sleep (5)
+
+
+## ----------------------------------------- ##
+#
+## ----------------------------------------- ##
+if __name__ == '__main__':
+  main(sys.argv)  
