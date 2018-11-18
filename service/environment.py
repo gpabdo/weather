a4f1dae3ab8ea998f27bbdc6bacc64cebb2ef531
gpabdo <gpabdo@gmail.com>
2018-11-18 00:23:39 +0000

    mod env

--- service/environment.py
+++ service/environment.py
@@ -2,29 +2,43 @@
 
 import json
 import time
-import socket
+import requests
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
 from arduino import arduinoSerial
 
 #
-# Setup UDP socket.
-#
-UDP_IP = "127.0.0.1"
-UDP_PORT = 8092
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-#
-#
+# Oh where is the ardunio?
 #
 arduino = arduinoSerial()
 
-data = {}
-data['command'] = 'getStatus'
+producer = KafkaProducer(bootstrap_servers=['kafka.pad.the-collective-group.com:9092'], value_serializer=lambda v: json.dumps(v).encode('ascii'))
 
+print "Starting loop"
 while( True ):
+
+   data = {}
+   event = {}
+
+   data['command'] = 'getParticles'
+   arduino.write(json.dumps(data))
+  
+   event['pm'] = json.loads(arduino.read())
+   if event['pm']["status"] != "OK":
+     print "ERROR: " + str( event['pm']["status"] )
+     continue 
+
+   data['command'] = 'getWeather'
    arduino.write(json.dumps(data))
 
-   object = json.loads(arduino.read())
-   MESSAGE = "environment,site=Ballard,location=Outside temperature=%s,humidity=%s,pressure=%s,light=%s,windDir=%s,windSpeed=%s,rain=%s,gust=%s" % (object['temp'] , object['humidity'], object['pressure'], object['light'], object['windDir'], object['windSpeed'], object['rain'], object['gust'] )
-   sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-#   print( MESSAGE )
-   time.sleep (1)
+   event['weather'] = json.loads(arduino.read())
+   if event['weather']["status"] != "OK":
+     print "ERROR: " + str( event['weather']["status"] )
+     continue
+
+   event["site"] = "Pad"
+   event["location"] = "Bedroom"
+   
+   producer.send('environment', event)
+
+   time.sleep (5)
